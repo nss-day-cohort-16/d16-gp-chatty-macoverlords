577e0db863c3fa8bc58426f4e6a31c3a0e07f11b
Rico <richardthomaswhitfield@gmail.com>
2016-10-27 11:22:51 -0500

    updated files

--- javascripts/chatty.js
+++ javascripts/chatty.js
@@ -1,15 +1,16 @@
 'use strict';
-// var Chatty = (function () {
-// 	var messages = [];
-// 	return {
-// 		loadMessages: function(callBack) {
-// 			var loader = new XMLHttpRequest();
-// 			loader.open("GET", "messages.json");
-// 			loader.send();
-// 			loader.addEventListener("load", function () {
-// 				messages = JSON.parse(this.responseText).messages;
-// 				callBack(messages);
-// 			});
-// 		}
-// 	};
-// })();
+var Chatty = (function () {
+	var messages = [];
+	return {
+		loadMessages: function(callBack) {
+			var loader = new XMLHttpRequest();
+			loader.open("GET", "messages.json");
+			loader.send();
+			loader.addEventListener("load", function () {
+				messages = JSON.parse(this.responseText).messages;
+				callBack(messages);
+			});
+		}
+	};
+})();
+console.log("chatty");
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,20 @@
 'use strict';
-function showMessage (messages) {
-	messages.forEach( function(item) {
-		document.getElementById("messages").innerHTML += "<li>" + item.message + "<button class='delete'>" + "Delete" + "</button></li>";
-	});
-}
+
+
+var Chatty = (function(oldChatty) {
+	var newMessage = [];
+
+	return {
+		returnMsg: function(messages) {
+			messages.forEach(function (item) { 
+			document.getElementById("messages").innerHTML += "<li>" + item.message + "<button class='delete'>" + "Delete" + "</button></li>";
+			});
+		}	
+	
+	};
+
+})(Chatty || {});
+
+// Chatty.loadMessages(returnMsg);
+
+console.log(Chatty);
