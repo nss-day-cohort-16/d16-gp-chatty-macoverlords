3ea0c244f8eb52d8763603900c8b69dd191741e7
Rico <richardthomaswhitfield@gmail.com>
2016-10-27 09:19:54 -0500

    include inital files for chatty project

---
+++ javascripts/chatty.js
@@ -0,0 +1,14 @@
+var Chatty = (function () {
+	var messages = [];
+	return {
+		loadMessages: function(callBack) {
+			var loader = new XMLHttpRequest();
+			loader.open("GET", "messages.json");
+			loader.send();
+			loader.addEventListener("load", function () {
+				messages = JSON.parse(this.responseText).messages;
+				callBack(messages);
+			});
+		}
+	};
+})();
---
+++ javascripts/main.js
@@ -0,0 +1,5 @@
+function showMessage (messages) {
+	messages.forEach( function(item) {
+		document.getElementById("messages").innerHTML += "<li>" + item.message + "<button class='delete'>" + "Delete" + "</button></li>";
+	});
+}
