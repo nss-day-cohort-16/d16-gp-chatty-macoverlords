87b5e23d516a0624fcbb718c562355dacd55c486
Nick Chemsak <nchemsak@gmail.com>
2016-10-27 11:49:18 -0500

    added event listeners JS file and linked in index

--- javascripts/chatty.js
+++ javascripts/chatty.js
@@ -1,15 +1,16 @@
 'use strict';
-// var Chatty = (function () {
-// 	var messages = [];
-// 	return {
-// 		loadMessages: function(callBack) {
-// 			var loader = new XMLHttpRequest();
-// 			loader.open("GET", "messages.json");
-// 			loader.send();
-// 			loader.addEventListener("load", function () {
-// 				messages = JSON.parse(this.responseText).messages;
-// 				callBack(messages);
-// 			});
-// 		}
-// 	};
-// })();
+var Chatty = (function () {
+	var messages = [];
+	return {
+		loadMessages: function(callBack) {
+			console.log("loadMessages running");
+			var loader = new XMLHttpRequest();
+			loader.addEventListener("load", function () {
+				messages = JSON.parse(this.responseText).messages;
+				callBack(messages);
+			});
+			loader.open("GET", "data/messages.json");
+			loader.send();
+		}
+	};
+})(Chatty || {});
