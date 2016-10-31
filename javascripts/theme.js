13d9b023b142c9dee2002cc8c805c68b4f8ad97c
Paul Martin <Dmart331@gmail.com>
2016-10-31 10:54:40 -0500

    added styling, makeDark

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -3,20 +3,26 @@ var theme = document.getElementById("darkTheme");
 var body =document.getElementById("body");
 var textLarge = document.getElementById("largeText");
 var nav = document.getElementById("nav");
+var wrapper = document.getElementById("messageArea");
+console.log("wrapper" , wrapper);
 
 theme.addEventListener("click" , makeDark);
 textLarge.addEventListener("click" , makeBig);
 
 function makeDark(event){
-
-    if(event){
-        body.classList.toggle("foo")
+console.log("click" , event);
+    if(event.target === theme){
+    	console.log("here?", wrapper , wrapper.classList)
+        wrapper.classList.toggle("makeDark");
     }
 };
 
 function makeBig(event){
-     if(event){
-         body.classList.toggle("fuh")
+     if(event.target === textLarge){
+     	console.log("click")
+         wrapper.classList.toggle("fuh")
+         
+         
      }
 };
 
