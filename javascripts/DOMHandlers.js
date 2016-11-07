94a40fb26f884b5cdeec232d2ef12bb4adc64ed6
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-07 09:09:08 -0600

    updated DOMhandlers.js

--- javascripts/DOMHandlers.js
+++ javascripts/DOMHandlers.js
@@ -1,29 +1,29 @@
 "use strict";
-/*DOM LISTENERS*/
+// Supressing JShint errors
+/* jshint -W097 */      // use strict within function only error
 
-document.body.addEventListener("click", function(event) {
 
-    if (event.target.className === "delete") {
-        var msgToDeleteId = event.target.id;
-        Chatty.removeMsg(event.target.parentNode, msgToDeleteId);
+document.body.addEventListener("click", event => {
 
+  if (event.target.className === "delete") {
+    var msgToDeleteId = event.target.id;
+    Chatty.removeMsg(event.target.parentNode, msgToDeleteId);
+
+  }
+
+  if (event.target.id === "clearBtn") {
+    while ($(".listMsg").length > 0) {
+      $(".listMsg")[0].parentNode.removeChild($(".listMsg")[0]);
+      output.value = "";
     }
-
-    if (event.target.id === "clearBtn") {
-        var listMsg = document.getElementsByClassName("listMsg");
-        while (listMsg.length > 0) {
-            listMsg[0].parentNode.removeChild(listMsg[0]);
-            output.value = "";
-        }
-    }
+  }
 });
 
-var output = document.getElementById("messageField");
-output.addEventListener("keypress", function(event) {
-    var key = event.which || event.keyCode;
-    if (key === 13) {
-        // console.log("enter", event);
-        Chatty.showMsg();
-        output.value = "";
-    }
+$("#messageField").keypress( event => {
+  var key = event.which || event.keyCode;
+  if (key === 13) {
+    Chatty.showMsg();
+    output.value = "";
+  }
+
 });
