88a0c2d39ef96314c26c79d782b132d7063b837d
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-09 10:16:40 -0600

    jquery-ified theme.js

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,36 +1,15 @@
 "use strict";
 
-var theme = document.getElementById("darkTheme");
-var body =document.getElementById("body");
-var textLarge = document.getElementById("largeText");
-var seizure = document.getElementById("seizure");
-var nav = document.getElementById("nav");
-var wrapper = document.getElementById("messageArea");
-var messages = document.getElementById("messages");
-console.log("wrapper" , wrapper);
+let wrapper = $("#messageArea");
 
-theme.addEventListener("click" , makeDark);
-textLarge.addEventListener("click" , makeBig);
+$("#darkTheme").click(function (){
+  wrapper.toggleClass("makeDark");
+});
 
-function makeDark(event){
-console.log("click" , event);
-    if(event.target === theme){
-    	console.log("here?", wrapper , wrapper.classList);
-        wrapper.classList.toggle("makeDark");
-    }
-}
+$("#largeText").click(function (){
+  $("#messages").toggleClass("fuh");
+});
 
-function makeBig(event){
-     // if(event.target === textLarge){
-      if(event) {
-     	// console.log("click")
-         messages.classList.toggle("fuh");
-     }
-}
-
-seizure.addEventListener("click", seizure1);
-function seizure1(event) {
-  if(event) {
-    wrapper.classList.toggle("nick");
-  }
-}
+$("#seizure").click(function (){
+  wrapper.toggleClass("nick");
+});
